3ff361ad7d30910eece1c73e281a1e348f7f25b8
vedrannovak <thevedrannovak@gmail.com>
2022-09-09 23:27:08 +0200

    V0101

---
+++ Poglavlje_01/Vjezbe/V0101.java
@@ -0,0 +1,18 @@
+/*
+ * Objektno-orijentirano programiranje u Javi.
+ * 
+ * Copyright © 2020 Vedran Novak
+ */
+
+/**
+ * Vježba V0101
+ * Kompajlirati i izvesti program te ispraviti prijavljene greške.
+ */
+public class V0101 {
+
+    public static void main(String[] args) {
+
+        System.out.pintln("Hi, World! Ovo je vježba V0101");
+    }
+
+}
