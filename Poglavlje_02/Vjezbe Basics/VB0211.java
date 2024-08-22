ea2cfd42bed4ea97804fcc6c94092ba602ac439a
Den1x95 <167130243+Den1x95@users.noreply.github.com>
2024-08-22 17:17:05 +0200

    Vjezbe Basics

---
+++ Poglavlje_02/Vjezbe Basics/VB0211.java
@@ -0,0 +1,28 @@
+
+public class VB0211 {
+
+	public static void main(String[] args) {
+		
+		    int a = 8;
+	        int b = 9;
+	        int c = 6;
+	        
+	       if (a > b && a > c) {
+	    	   System.out.println ("Najveci broj je " + a);
+	    	   
+	    	   
+	       }
+	       else if(b > c) {
+	    	   System.out.println("Najveci broj je " + b);
+	    	   
+	    	   
+	       }
+	       else {
+	    	   System.out.println("Najveci broj je " + c);
+	       }
+		
+
+	}
+}
+
+
---
+++ Poglavlje_02/Vjezbe Basics/VB0212.java
@@ -0,0 +1,33 @@
+
+public class VB0212 {
+
+	public static void main(String[] args) {
+
+	    int a = 11;
+        int b = 12;
+        int c = 13;
+        int d = 14;
+        
+        
+       if (a > b && a > c) {
+    	   System.out.println ("Najveci broj je " + a);
+    	   
+    	   
+       }
+       else if(b > c && b > d) {
+    	   System.out.println("Najveci broj je " + b);
+    	   
+    	   
+       }
+       else if (c > d){
+    	   System.out.println("Najveci broj je " + c);
+       }
+       else {
+    	   System.out.println("Najveci broj je " + d);
+    	   
+
+}
+
+	}
+
+}
