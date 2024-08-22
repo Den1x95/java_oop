70a66523cda6558843f3c426752d4af9de646501
Den1x95 <167130243+Den1x95@users.noreply.github.com>
2024-08-22 17:41:38 +0200

    Update

--- Poglavlje_02/Vjezbe Basics/VB0212.java
+++ Poglavlje_02/Vjezbe Basics/VB0212.java
@@ -3,30 +3,46 @@ public class VB0212 {
 
 	public static void main(String[] args) {
 
-	    int a = 11;
-        int b = 12;
-        int c = 13;
-        int d = 14;
+	    int a = 58;
+        int b = 60;
+        int c = 1;
+        int d = 80;
+        int max;
+        int min;
+       
         
         
-       if (a > b && a > c) {
-    	   System.out.println ("Najveci broj je " + a);
-    	   
-    	   
+        
+        
+       if (a > b && a > c )  {
+    	  max = a;
        }
-       else if(b > c && b > d) {
-    	   System.out.println("Najveci broj je " + b);
-    	   
-    	   
+       
+       else if(b > c && b > d)  {
+    	 max = b; 
        }
-       else if (c > d){
-    	   System.out.println("Najveci broj je " + c);
+        
+       else if (c > d ){
+    	   max = c;
        }
+       
        else {
-    	   System.out.println("Najveci broj je " + d);
-    	   
-
-}
+    	  max = d ;
+       }
+       
+       System.out.println("Najveci br je: " + max );
+       
+       
+       min = (a < b && a < c ) ? a : b;
+       min = (a < b && a < c ) ? a : c;
+       min = (b < c && b < d ) ? b:c ;
+       min = (a < d )? a: c;
+       min = (b < a)? b:a ;
+       min = (b < d)? b:d;
+       min = (c < d)? c:d; 
+       
+      
+       System.out.println("Najmanj broj je: " + min);
 
 	}
 
