2f1f2f610048c108e250d9a61fa3657647e2bb47
Den1x95 <167130243+Den1x95@users.noreply.github.com>
2024-09-11 18:27:51 +0200

    VB0214

---
+++ Poglavlje_02/Vjezbe Basics/VB0214.java
@@ -0,0 +1,29 @@
+
+public class VB0214 {
+
+	public static void main(String[] args) {
+		
+		
+		float a = 8.6F;
+		float b = 4.3F;
+		
+		float c = a / b ;
+		
+		
+		  
+		if (b == 0 ) {
+			
+			System.out.println( "Rezultat nije broj " );
+			
+		}
+		else if (b > 0 ) {
+			System.out.println("Rezultat djeljenja je " + c );
+		}
+		
+		
+		
+
+
+	}
+
+}
